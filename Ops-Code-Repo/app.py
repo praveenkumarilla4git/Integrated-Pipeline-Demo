d85b24e996eb87748bed07c1a645e27cc43a6467
praveenkumarilla4git <praveenkumarilla@gmail.com>
2025-12-27 19:15:54 +0530

    Switched back to Flask web server

--- Ops-Code-Repo/app.py
+++ Ops-Code-Repo/app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Get the current time dynamically
+    # Get current time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    # Render the HTML template and pass the time variable
+    # Render the HTML template (make sure templates/index.html exists!)
     return render_template('index.html', timestamp=current_time)
 
 if __name__ == '__main__':
+    # This keeps the server running forever!
     app.run(host='0.0.0.0', port=5000)
